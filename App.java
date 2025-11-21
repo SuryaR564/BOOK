a83113309bbef1d6e69f7e36b5f54e75d3c82340
SuryaR564 <surya297244@gmail.com>
2025-11-21 10:54:01 -0800

    Add files via upload

---
+++ App.java
@@ -0,0 +1,9 @@
+class App {
+  public static void main(String[] args) {
+    Book aBook = new Book("https://www.gutenberg.org/cache/epub/6130/pg6130.txt");
+    //System.out.println(aBook.pigLatin("hello"));
+    // System.out.println(aBook.translateWord("What?!?"));
+    // System.out.println(aBook.translateWord("Allons-y"));
+
+  }
+}
